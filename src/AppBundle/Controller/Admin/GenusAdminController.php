dc1ba6422749af4c91a33903e50da67d7f091bbb
chillfont <chillfont@gmail.com>
2019-08-21 17:20:18 +0700

    The form works great

---
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\Genus;
+use AppBundle\Form\GenusFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/admin")
+ */
+class GenusAdminController extends Controller
+{
+    /**
+     * @Route("/genus", name="admin_genus_list")
+     */
+    public function indexAction()
+    {
+        $genuses = $this->getDoctrine()
+            ->getRepository('AppBundle:Genus')
+            ->findAll();
+
+        return $this->render('admin/genus/list.html.twig', array(
+            'genuses' => $genuses
+        ));
+    }
+
+    /**
+     * @Route("/genus/new", name="admin_genus_new")
+     */
+    public function newAction(Request $request)
+    {
+    	$form = $this->createForm(GenusFormType::class);
+
+        // only handles data on POST
+    	$form->handleRequest($request);
+
+    	if ($form->isSubmitted() && $form->isValid()) {
+            $genus = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            $this->addFlash('success', 'Genus created - you are amazing!');
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
+
+    	return $this->render('admin/genus/new.html.twig', [
+    		'genusForm' => $form->createView()
+	    ]);
+    }
+
+    /**
+     * @Route("/genus/{id}/edit", name="admin_genus_edit")
+     * @param Request $request
+     * @param Genus $genus
+     * @return RedirectResponse|Response
+     */
+	public function editAction(Request $request, Genus $genus)
+	{
+
+
+        $form = $this->createForm(GenusFormType::class, $genus);
+
+        // only handles data on POST
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $genus = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            $this->addFlash('success', 'Genus updated - you are amazing!');
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
+
+        return $this->render('admin/genus/edit.html.twig', [
+            'genusForm' => $form->createView()
+        ]);
+	}
+}
